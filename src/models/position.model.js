52e1cd49cc71782f4b0d638722c25836eefa1bd1
Hugo Carreira <hugo.colombo23@gmail.com>
2018-12-07 16:39:26 -0200

    :construction_worker: Position model

---
+++ src/models/position.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const SCHEMA = mongoose.Schema
+
+const POSITION_SCHEMA = new SCHEMA({
+    title: { type: String },
+    description: { type: String },
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+}, { collection: 'positions' })
+
+
+const Position = mongoose.model('People', POSITION_SCHEMA)
+module.exports = Position
